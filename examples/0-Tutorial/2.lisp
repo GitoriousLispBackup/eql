a86876ea4317f24fe57997b1d68e9fc92f9e7065
me <polos.ruetz@gmail.com>
2010-11-19 20:12:50 +0100

    re-add unintentionally deleted "2.lisp" from Qt Widgets Tutorial

---
+++ examples/0-Tutorial/2.lisp
@@ -0,0 +1,12 @@
+;;; Ported Qt Widgets Tutorial - Child Widgets
+
+(in-package :eql)
+
+(let ((window (qnew "QWidget"
+                    "size" (list 320 240)
+                    "windowTitle" "Child widget"))
+      (button (qnew "QPushButton"
+                    "text" "Press me"
+                    "pos" (list 100 100))))
+  (qfun button "setParent" window)
+  (qfun window "show"))
