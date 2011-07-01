15f1f22061cf52568fcb8a224d0fc64595ab7a2a
me <polos.ruetz@gmail.com>
2011-07-01 14:44:20 +0200

    Example 6 (download): add message-box (success/error)

--- examples/6-download.lisp
+++ examples/6-download.lisp
@@ -22,15 +22,31 @@
 
 (defun download-finished (reply)
   (qfun reply "deleteLater") ; QNetworkReply*: heap result, delete manually
-  (let ((data (qfun reply "readAll")))
-    (save-data data)
-    (format t "~%Downloaded ~D bytes, see \"download.data\".~%~%" (length data))
-    (qq)))
+  (let ((error (qfun reply "error")))
+    (if (= |QNetworkReply.NoError| error)
+        (let ((data (qfun reply "readAll")))
+          (save-data data)
+          (qfun "QMessageBox" "information" nil "EQL"
+                (format nil (tr "~%Downloaded ~:D bytes, see \"download.data\".~%~%") (length data))))
+        (show-error error)))
+  (qq))
 
 (defun save-data (data)
   (with-open-file (s "download.data" :direction :output :if-exists :supersede
                      :element-type '(signed-byte 8))
     (write-sequence data s)))
+
+(defun show-error (error)
+  (let (msg) 
+    (do-external-symbols (symbol)
+      (let ((name (symbol-name symbol)))
+        (when (and (x:starts-with "QNetworkReply." name)
+                   (x:ends-with "Error" name)
+                   (= error (symbol-value symbol)))
+          (setf msg (format nil (tr "Download error: <span style='color:red; font-weight:bold;'>~A</span>")
+                            (subseq name (1+ (position #\. name)) (- (length name) (length "Error")))))
+          (return))))
+    (qfun "QMessageBox" "critical" nil "EQL" (if msg msg (tr "Unkown download error.")))))
 
 (progn
   (ini)
