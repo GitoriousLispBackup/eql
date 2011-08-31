29626632d9539ecebd64d9f631c7e5e4bbbf2bcd
me <polos.ruetz@gmail.com>
2011-08-31 16:36:37 +0200

    tiny revision

--- slime/process-qt-events.lisp
+++ slime/process-qt-events.lisp
@@ -1,23 +1,6 @@
 ;;; code taken from "swank-backend.lisp"
 
-(ignore-errors (make-package :swank-backend))
-
 (in-package :swank-backend)
-
-(defparameter *interface-functions*      nil)
-(defparameter *unimplemented-interfaces* nil)
-
-(defmacro defimplementation (name args &body body)
-  (assert (every #'symbolp args) ()
-          "Complex lambda-list not supported: ~S ~S" name args)
-  `(progn
-     (setf (get ',name 'implementation)
-           ;; For implicit BLOCK. FLET because of interplay w/ decls.
-           (flet ((,name ,args ,@body)) #',name))
-     (when (member ',name *interface-functions*)
-           (setq *unimplemented-interfaces*
-                 (remove ',name *unimplemented-interfaces*)))
-     ',name))
 
 (defimplementation wait-for-input (streams &optional timeout)
   (loop
