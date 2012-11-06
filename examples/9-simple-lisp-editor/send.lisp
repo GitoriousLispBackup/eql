9339e946c7d0fec289a10006b1b1ccaff759130d
me <polos.ruetz@gmail.com>
2012-11-06 23:42:40 +0100

    example 9: update "send.lisp"

--- examples/9-simple-lisp-editor/send.lisp
+++ examples/9-simple-lisp-editor/send.lisp
@@ -1,8 +1,68 @@
-;;; Send Lisp expressions to "local-server" (no EQL and no X server needed).
+;;; copyright (c) 2012 Polos Ruetz
 ;;;
-;;; Example:
-;;;   (! (qmsg :test))
+;;; N.B: This is both trivial and experimental!
+;;;
+;;; Send EQL code to "local-server".
+;;; Allows a (very limited) use of EQL from any Common Lisp.
+;;;
+;;; Build:
+;;;
+;;;   in "send/" do: qmake && make
+;;;   eql make-local-server-fasl.lisp
+;;;
+;;; Run:
+;;;
+;;;   eql eql-local-server.fas
+;;;   ecl -load send.lisp / sbcl --load send.lisp
+;;;
+;;; Examples:
+;;;
+;;;   #q (qmsg (package-name *package*))
+;;;
+;;;   #q (qmsg #.(package-name *package*)) ; pass any data
+;;;
+;;;   #q (load "../2-clock.lisp")
+;;;   #q (qfun clock::*clock* "showMaximized")
 
-(defmacro ! (exp)
-  (ext:run-program "send/send" (list (prin1-to-string exp))))
+(set-dispatch-macro-character #\# #\q (lambda (s c n) (%read-q s)))
+
+(defun %read-q (in)
+  (list '%q (with-output-to-string (out)
+              (let ((ex #\Space)
+                    parens in-string)
+                (loop
+                  (let ((ch (read-char in)))
+                    (write-char ch out)
+                    (when (and (char=  #\" ch)
+                               (char/= #\\ ex))
+                      (setf in-string (not in-string)))
+                    (unless in-string
+                      (case ch
+                        (#\( (if parens (incf parens) (setf parens 1)))
+                        (#\) (decf parens)))
+                      (when (and parens (zerop parens))
+                        (return)))
+                    (setf ex ch)))))))
+
+(defmacro while-it (exp &body body)
+  `(let (it)
+     (do ()
+       ((not ,(list 'setf 'it exp)))
+       ,@body)))
+
+(defun insert-data (string)
+  (while-it (search "#." string)
+    (multiple-value-bind (exp end)
+      (read-from-string (subseq string (+ it 2)))
+      (setf string (concatenate 'string
+                                (subseq string 0 it)
+                                (prin1-to-string (eval exp))
+                                " "
+                                (subseq string (+ it 2 end))))))
+  string)
+
+(defun %q (string)
+  (#+ecl  ext:run-program
+   #+sbcl sb-ext:run-program
+   "send/send" (list (insert-data string))))
 
