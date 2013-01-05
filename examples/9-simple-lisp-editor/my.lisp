4255a17c6397995e64e6fecc67ca3b6d4e985ad7
me <polos.ruetz@gmail.com>
2013-01-05 23:54:43 +0100

    (sorry)

--- examples/9-simple-lisp-editor/my.lisp
+++ examples/9-simple-lisp-editor/my.lisp
@@ -1,2 +1,29 @@
+(in-package :eql-user)
 
-(defun
+(defvar *window*    (qnew "QDialog"))
+(defvar *label*     (qnew "QLabel"))
+(defvar *line-edit* (qnew "QLineEdit"))
+(defvar *layout*    (qnew "QVBoxLayout"))
+
+(defun start ()
+  (qset *window* "windowTitle" "My Personal Analyst")
+  (qset *label* "text" "Please enter your <b>Top Secret</b> (you will feel better)")
+  (qset *line-edit* "echoMode" |QLineEdit.Password|)
+  (dolist (w (list *label* *line-edit*))
+    (qfun *layout* "addWidget" w))
+  (qfun *window* "setLayout" *layout*)
+  (qconnect *line-edit* "returnPressed()" 'analyze)
+  (x:do-with (qfun *window*) "show" "raise"))
+
+(defun analyze ()
+  (qlet ((dlg "QProgressDialog"
+              "maximum" 20
+              "labelText" "<h1 style='color:crimson'>Wow!</h1><p>(updating WikiLeaks...)</p>"))
+    (x:do-with (qfun dlg) "show" "raise")
+    (dotimes (n (qget dlg "maximum"))
+      (sleep 0.1)
+      (qset dlg "value" n)
+      (qprocess-events)))
+  (qfun *line-edit* "clear"))
+
+(start)
