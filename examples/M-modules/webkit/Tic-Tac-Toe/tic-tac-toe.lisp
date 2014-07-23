4d33f1ab91dc64781f1ae628cf73ae601224d392
me <polos.ruetz@gmail.com>
2014-07-23 17:39:01 +0200

    example "tic-tac-toe.lisp": add computer player

--- examples/M-modules/webkit/Tic-Tac-Toe/tic-tac-toe.lisp
+++ examples/M-modules/webkit/Tic-Tac-Toe/tic-tac-toe.lisp
@@ -11,7 +11,9 @@
    #:*board*
    #:*cells*
    #:*cell-count*
+   #:*computer-player*
    #:*new-game*
+   #:*win-rows*
    #:move
    #:new-game))
 
@@ -34,15 +36,23 @@
 (defvar *cells*    "[class='cells']")
 (defvar *new-game* "#new-game")
 
-(defvar *cell-count* 9)
+(defvar *computer-player* t)
+(defvar *cell-count*      9)
 
 (defun cell-id (index)
   (format nil "#c~D" (1+ index)))
 
+(defun cell-text (index)
+  (h:hget (cell-id index) :text))
+
+(defun pretty-binary (number)
+  (reverse (substitute #\. #\0 (format nil "~9,'0B" number))))
+
 (defun ini-html ()
   (dotimes (i *cell-count*)
     (h:hset (cell-id i)
-            :class "cells"))
+            :class "cells"
+            :binary-position (pretty-binary (s i)))) ; dynamic property
   (h:hset *cells*
           :onclick (h:lisp (move :this))) ; pass QWebElement as JS 'this'
   (h:hset *new-game*
@@ -51,6 +61,8 @@
 (let ((s ""))
   (defun x-o ()
     (setf s (if (string= "X" s) "O" "X")))
+  (defun previous-x-o ()
+    s)
   (defun reset-x-o ()
     (setf s "")))
 
@@ -63,7 +75,10 @@
   (when (and (not (won))
              (x:empty-string (h:hget web-element :text)))
     (h:hset web-element :text (x-o))
-    (check-win)))
+    (check-win)
+    (when (and (not (won))
+               *computer-player*
+      (computer-move)))))
 
 (defun s (&rest numbers)
   "Sum of binary shifted numbers."
@@ -73,20 +88,29 @@
                          (s 0 3 6) (s 1 4 7) (s 2 5 8) ; vertical
                          (s 0 4 8) (s 2 4 6)))         ; X
 
+(defun state (x-o)
+  (let ((sum 0))
+    (dotimes (i *cell-count*)
+      (when (string= x-o (cell-text i))
+        (incf sum (s i))))
+    sum))
+
 (defun check-win ()
-  (let ((values (multiple-value-list (h:hget *cells* :text))))
-    (dolist (xo '("X" "O"))
-      (let ((sum 0))
-        (loop :for val :in values
-              :for i :upfrom 0
-              :do (when (string= xo val)
-                    (incf sum (s i))))
-        (dolist (row *win-rows*)
-          (when (= row (logand sum row)) ; bit logic
-            (mark-row row)
-            (blink-row)
-            (add-to-history)
-            (return-from check-win)))))))
+  (dolist (x-o '("X" "O"))
+    (let ((state (state x-o)))
+      (dolist (row *win-rows*)
+        (when (= row (logand state row)) ; bit logic
+          (print-win-state row)
+          (mark-row row)
+          (blink-row)
+          (add-to-history)
+          (return-from check-win t))))))
+
+(defun print-win-state (row)
+  (format t "~%~A~%~A~%"
+          (map 'string (lambda (s) (if (x:empty-string s) #\. (char s 0)))
+               (multiple-value-list (h:hget *cells* :text)))
+          (pretty-binary row)))
 
 (defun set-background-color (i color)
   (h:set-style-property (cell-id i) :background-color color))
@@ -122,4 +146,39 @@
   (defun won ()
     marked))
 
-(ini "tic-tac-toe.htm" '(350 450))
+;;; computer player
+
+(defun check-win-move (x-o)
+  "Find missing position for 3-in-a-row."
+  (let ((state (state x-o)))
+    (dolist (row *win-rows*)
+      (let ((i (position (- row (logand state row)) ; bit logic
+                         #.(quote (list (s 0) (s 1) (s 2) (s 3) (s 4) (s 5) (s 6) (s 7) (s 8))))))
+        (when (and i (x:empty-string (cell-text i)))
+          (return-from check-win-move i))))))
+
+(defun computer-move ()
+  (qsleep 1/4)
+  (flet ((mv (i)
+           (when (and i (x:empty-string (cell-text i)))
+             i))
+         (rnd (nums)
+           (x:when-it (remove-if-not (lambda (i) (x:empty-string (cell-text i)))
+                                     nums)
+             (nth (random (length x:it)) x:it))))
+    (let ((prev (previous-x-o))
+          (curr (x-o)))
+      ;; not too clever, human can win
+      (x:when-it (or (check-win-move curr)
+                     (check-win-move prev)
+                     (mv 4)
+                     (mv (rnd '(0 2 6 8)))
+                     (mv (rnd '(1 3 5 7))))
+        (h:hset (cell-id x:it) :text curr)
+        (check-win)))))
+
+;;; run
+
+(progn
+  (ini "tic-tac-toe.htm" '(350 450))
+  (qsingle-shot 0 (lambda () (in-package :tic-tac-toe))))
