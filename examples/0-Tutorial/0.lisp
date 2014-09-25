57efdceb03f87d424bd03901c250f655e62c3871
me <polos.ruetz@gmail.com>
2014-09-25 15:52:51 +0200

    add tutorial example "4.wrappers.lisp"; add wrapper versions in tutorial example "0.lisp"

--- examples/0-Tutorial/0.lisp
+++ examples/0-Tutorial/0.lisp
@@ -1,8 +1,18 @@
-;;; Lisp versus C++ (note reversed order)
+;; Lisp versus C++ (note reversed order)
 
 (! "currentTime" "QTime")                               ; QTime::currentTime()
 
 (! ("toString" "currentTime" "QTime"))                  ; QTime::currentTime().toString()
 
 (! ("toString" ("addSecs" 1000) "currentTime" "QTime")) ; QTime::currentTime().addSecs(1000).toString()
+
+;; ----------------------------------------------------------------------------------------------------
+
+;; using Qt wrappers (see "src/lisp/all-wrappers.lisp")
+
+(|currentTime.QTime|)
+
+(|toString| (|currentTime.QTime|))
+
+(|toString| (|addSecs| (|currentTime.QTime|) 1000))
 
